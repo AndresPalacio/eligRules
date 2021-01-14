335789517e7c62a04bf2c211f50134e0fa6072cf
osboxes.org <mikah.geek@gmail.com>
2021-01-14 15:47:36 -0500

    some sanity tests, and TestContext for correctly running the Handler

--- execution/src/main/java/com/r1/eligRules/Handler.java
+++ execution/src/main/java/com/r1/eligRules/Handler.java
@@ -2,25 +2,20 @@ package com.r1.eligRules;
 
 import com.r1.eligRules.examples.decisiontable.*;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.Map;
 
 import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 public class Handler implements RequestHandler<Map<String,String>, String> {
 
-    static final Logger LOG = LoggerFactory.getLogger(Handler.class);
     Gson gson = new GsonBuilder().setPrettyPrinting().create();
     
     public Handler() {
@@ -28,7 +23,8 @@ public class Handler implements RequestHandler<Map<String,String>, String> {
 
     @Override
     public String handleRequest(Map<String,String> event, Context context) {
-        LOG.info("in handleRequest, event = " + event.toString());
+        LambdaLogger logger = context.getLogger();
+        logger.log("in handleRequest, event = " + event.toString());
         KieServices ks = KieServices.Factory.get();
         KieContainer kContainer = ks.getKieClasspathContainer();
         KieSession ksession = kContainer.newKieSession("TestKS");
@@ -36,8 +32,6 @@ public class Handler implements RequestHandler<Map<String,String>, String> {
         ksession.insert(p);
         ksession.fireAllRules();
         ksession.dispose();
-        LOG.info("handleRequest completed");
-        LOG.info("valid = " + p.getValid());
-        return null;
+        return Boolean.toString(p.getValid());
     }
 }
---
+++ execution/src/test/java/com/r1/eligRules/HandlerTests.java
@@ -0,0 +1,43 @@
+package com.r1.eligRules;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.google.gson.GsonBuilder;
+import com.r1.eligRules.examples.decisiontable.Person;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HandlerTests {
+
+    static final Logger LOG = LoggerFactory.getLogger(TestContext.class);
+
+    @Test
+    public void test_sanity_personAgeOld() {
+        Handler h = new Handler();
+        TestContext context = new TestContext();
+        Map<String, String> obj = new HashMap<String, String>();
+        obj.put("age", "999");
+        String res = h.handleRequest(obj, context);
+        context.getLogger().log(res);
+        assertTrue("age is old", Boolean.parseBoolean(res));
+        
+    }
+
+    @Test
+    public void test_sanity_personAgeYoung() {
+        Handler h = new Handler();
+        TestContext context = new TestContext();
+        Map<String, String> obj = new HashMap<String, String>();
+        obj.put("age", "1");
+        String res = h.handleRequest(obj, context);
+        context.getLogger().log(res);
+        assertFalse("age is young", Boolean.parseBoolean(res));
+        
+    }
+}
