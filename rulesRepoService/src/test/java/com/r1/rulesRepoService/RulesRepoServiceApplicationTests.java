364343722849e6b7e2867e57834d27073e714700
osboxes.org <mikah.geek@gmail.com>
2021-01-15 14:37:52 -0500

    init rulesRepoService. no drools/kie yet, no controllers either

---
+++ rulesRepoService/src/test/java/com/r1/rulesRepoService/RulesRepoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.r1.rulesRepoService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RulesRepoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
